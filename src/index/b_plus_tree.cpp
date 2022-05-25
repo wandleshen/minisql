e6563f8a0f65226d7430393b49c6e4d3fca59cc8
wandleshen <wandleshen@users.noreply.github.com>
2022-05-25 10:11:11 +0800

    Fix a bug related to #4

--- src/index/b_plus_tree.cpp
+++ src/index/b_plus_tree.cpp
@@ -13,7 +13,9 @@ BPLUSTREE_TYPE::BPlusTree(index_id_t index_id, BufferPoolManager *buffer_pool_ma
           comparator_(comparator),
           leaf_max_size_(leaf_max_size),
           internal_max_size_(internal_max_size) {
-
+  auto header_page = reinterpret_cast<IndexRootsPage *>(buffer_pool_manager_->FetchPage(INDEX_ROOTS_PAGE_ID)->GetData());
+  header_page->GetRootId(index_id_, &root_page_id_);
+  buffer_pool_manager_->UnpinPage(0, false);
 }
 
 INDEX_TEMPLATE_ARGUMENTS
@@ -532,7 +534,7 @@ Page *BPLUSTREE_TYPE::FindLeafPage(const KeyType &key, bool leftMost) {
  */
 INDEX_TEMPLATE_ARGUMENTS
 void BPLUSTREE_TYPE::UpdateRootPageId(int insert_record) {
-  IndexRootsPage *header_page = reinterpret_cast<IndexRootsPage *>(buffer_pool_manager_->FetchPage(0));
+  IndexRootsPage *header_page = reinterpret_cast<IndexRootsPage *>(buffer_pool_manager_->FetchPage(INDEX_ROOTS_PAGE_ID)->GetData());
   if (insert_record)
     // 1:新建一个root
     header_page->Insert(index_id_, root_page_id_);
