64e6739e7d00277e2c0060fd7b4efc87d8e55161
singing-cat <96773624+singing-cat@users.noreply.github.com>
2022-06-21 15:03:01 +0800

    Update bitmap_page.cpp

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -2,12 +2,12 @@
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  if (page_allocated_ >= GetMaxSupportedSize())
+  if (page_allocated_ >= GetMaxSupportedSize())		//超过最大范围则无法分配空闲页，返回false
     return false;
   page_allocated_++;
-  page_offset = next_free_page_;
+  page_offset = next_free_page_;	//偏移量移到下一个空闲页上
   bytes[page_offset / 8] |= (1 << (page_offset % 8));
-  for (size_t i = 0; i < GetMaxSupportedSize(); i++)
+  for (size_t i = 0; i < GetMaxSupportedSize(); i++)	//从中找出第一个空闲页
     if (IsPageFree(i)) {
       next_free_page_ = i;
       break;
@@ -17,9 +17,9 @@ bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  if (IsPageFree(page_offset))
+  if (IsPageFree(page_offset))	//若该页本身就是空闲页则无法回收
     return false;
-  page_allocated_--;
+  page_allocated_--;	//已分配页数减一
   bytes[page_offset / 8] &= ~(1 << (page_offset % 8));
   next_free_page_ = page_offset;
   return true;
@@ -29,7 +29,7 @@ template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
   uint32_t byte_index = page_offset / 8;
   uint8_t bit_index = page_offset % 8;
-  return IsPageFreeLow(byte_index, bit_index);
+  return IsPageFreeLow(byte_index, bit_index);	//通过下面的函数判断
 }
 
 template<size_t PageSize>
