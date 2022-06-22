be8d18660df5552e65aad3d3ce2eb0f29ac49837
PureSnow515 <87529614+PureSnow515@users.noreply.github.com>
2022-06-22 11:48:36 +0800

    Update row.cpp

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -8,16 +8,16 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
   uint32_t offset = 0;
   vector<bool> bits;
   for (auto field : fields_) {
-    bits.emplace_back() = field->IsNull();
+    bits.emplace_back() = field->IsNull();//将Row中空的field通过位图的方式进行记录
   }
   for (auto i : bits) {
-    MACH_WRITE_TO(bool, buf + offset, i);
+    MACH_WRITE_TO(bool, buf + offset, i);//将Row中空的field所推进的字节加到buf中
     offset += sizeof(bool);
   }
   for (auto field : fields_) {
     if (!field->IsNull()) {
       field->SerializeTo(buf + offset);
-      offset += field->GetSerializedSize();
+      offset += field->GetSerializedSize();//将Row中非空的field进行序列化
     }
   }
   return offset;
@@ -30,15 +30,15 @@ uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
   uint32_t offset = 0;
   vector<bool> bits;
   for (uint32_t i = 0; i < schema->GetColumnCount(); i++) {
-    bits.push_back(MACH_READ_FROM(bool, buf + offset));
-    offset += sizeof(bool);
+    bits.push_back(MACH_READ_FROM(bool, buf + offset));//从buf中反序列化的空的field，
+    offset += sizeof(bool);//给offset加上需要的字节
   }
   for (unsigned long i = 0; i < bits.size(); i++) {
     void* tmp = heap_->Allocate(sizeof(Field));
-    auto* field_tmp = new(tmp)Field(schema->GetColumn(i)->GetType());
-    Field::DeserializeFrom(buf + offset, schema->GetColumn(i)->GetType(), &field_tmp, bits[i], heap_);
+    auto* field_tmp = new(tmp)Field(schema->GetColumn(i)->GetType());//得到所要反序列化的数据类型
+    Field::DeserializeFrom(buf + offset, schema->GetColumn(i)->GetType(), &field_tmp, bits[i], heap_);//将field反序列化
     fields_.push_back(field_tmp);
-    offset += fields_.back()->GetSerializedSize();
+    offset += fields_.back()->GetSerializedSize();//给offset加上所需要字节
   }
   return offset;
 }
@@ -49,7 +49,7 @@ uint32_t Row::GetSerializedSize(Schema *schema) const {
   uint32_t size = 0;
   for (auto field : fields_) {
     if (!field->IsNull())
-      size += field->GetSerializedSize();
+      size += field->GetSerializedSize();//加上每个非空field的字节
   }
   size += fields_.size() * sizeof(bool);
   return size;
